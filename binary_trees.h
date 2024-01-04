9fef599cbdf900c35b22e1c580217b9355748037
KillerA-z1 <Ahmedz1a2m@gmail.com>
2024-01-05 00:52:04 +0300

    function that creates a binary tree node.

--- binary_trees.h
+++ binary_trees.h
@@ -20,10 +20,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -34,4 +34,6 @@ typedef struct binary_tree_s avl_t;
 /* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
+/* list of Functions that are used in mandatory task  */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 #endif /* _BINARY_TREES_H_ */
