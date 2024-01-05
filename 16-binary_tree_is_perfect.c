a71756e119e3aef86eb584c8d97e9b7ab6b391b0
KillerA-z1 <Ahmedz1a2m@gmail.com>
2024-01-05 17:33:41 +0300

    Hot fix 05

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+int power(int x, int y);
 
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
@@ -8,13 +9,18 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	size_t height, size;
+
 	if (!tree)
 		return (0);
 
 	if (!tree->left && !tree->right)
 		return (1);
 
-	return ((1ULL << (binary_tree_height(tree) + 1)) - 1 == (size_t)binary_tree_size(tree));
+	height = binary_tree_height(tree);
+	size = binary_tree_size(tree);
+
+	return ((1ULL << (height + 1)) - 1 == size);
 }
 
 /**
@@ -29,26 +35,28 @@ int power(int x, int y)
 	int result = 1;
 
 	if (y < 0)
-		return -1;
+		return (-1);
 
 	for (; y > 0; y--)
 		result *= x;
 
-	return result;
+	return (result);
 }
 
 /**
  * binary_tree_size - measures the size of a binary tree
  * @tree: tree to measure the size of
  *
- * Return: size of the tree, 0 if tree is NULL
+ * Return: 0 if tree is NULL else size of the tree.
+ *
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
-		return 0;
+		return (0);
 
-	return binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1;
+	return ((binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1));
 }
 
 /**
@@ -62,9 +70,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_l, height_r;
 
 	if (!tree)
-		return 0;
+		return (0);
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+	return ((height_l > height_r ? height_l : height_r));
 }
